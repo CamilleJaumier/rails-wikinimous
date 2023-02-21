1db03385b9df8f9b1c813ce2367db3e45c67c036
Camille Jaumier <camille.jaumier@icloud.com>
2023-02-21 16:52:33 +0100

    wikinimous

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # AFFICHER TOUS LES ARTICLES
+  get '/articles', to: 'articles#index'
+  # CREATE
+  get '/articles/new', to: 'articles#new', as: :article_new
+  post '/articles', to: 'articles#create'
+  # EDIT - UPDATE
+  get '/articles/:id/edit', to: 'articles#edit', as: :article_edit
+  patch '/articles/:id', to: 'articles#update'
+  # DELETE
+  delete '/articles/:id', to: 'articles#destroy'
+  # SEE ONE ARTICLE
+  get '/articles/:id', to: 'articles#show', as: :article
 end
